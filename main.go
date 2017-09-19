4b01fe97fe5516e21d5e9e0ad1ab562ccd5559b0
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-09-19 22:13:48 +0800

    321

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			prof := event.Source.UserID
-			fmt.Println(prof)
+			// fmt.Println(prof)
 			if _, err := bot.PushMessage(prof, linebot.NewTextMessage("Hello, world"+prof)).Do(); err != nil {
 
 					log.Print(err)
