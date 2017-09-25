1e23d76f7143eb88a1c271f804288193567b24ce
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-09-25 18:07:41 +0800

    <3

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
 	log.Println("Bot:", bot, " err:", err)
 	for t :=0 ; t<5 ; t++ {
-		time.NewTimer(time.Minute * 2)
+		time.Sleep(time.Minute * 2)
 		bot.PushMessage("Uecc089487f1487a78637be4e2fe3dca9", linebot.NewTextMessage("你好呀!")).Do()
 	}
 	http.HandleFunc("/callback", callbackHandler)
