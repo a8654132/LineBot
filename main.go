4c8efbddb9b8fffd0c4a2fb3dcbcdf06e5bdea0d
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-09-25 16:52:00 +0800

    87主動

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			for i:=0;i < len(user.USER) ; i++{
 				allcontent = allcontent+user.USER[i].CONTENT
 			}
-			if _, err := bot.PushMessage("Uecc089487f1487a78637be4e2fe3dca9", linebot.NewTextMessage(allcontent)).Do(); err != nil {
+			if _, err := bot.PushMessage("Uecc089487f1487a78637be4e2fe3dca9", linebot.NewTextMessage("你好!"+allcontent)).Do(); err != nil {
 					log.Print(err)
 			}
 		}
