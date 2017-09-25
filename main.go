9d738a74c6b1054c7fbf5e748307c4e6fe13231a
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-09-25 17:56:06 +0800

    不要這樣:(

--- main.go
+++ main.go
@@ -27,9 +27,7 @@ func main() {
 
 func setTime(){
 			time.NewTimer(time.Second * 2)
-			if _, err := bot.PushMessage("Uecc089487f1487a78637be4e2fe3dca9", linebot.NewTextMessage("你好呀!")).Do(); err != nil {
-					log.Print(err)
-			}
+			bot.PushMessage("Uecc089487f1487a78637be4e2fe3dca9", linebot.NewTextMessage("你好呀!")).Do()
 	}
 
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
