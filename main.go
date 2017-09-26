2e30ba1ece7696d1125cd8c9781b6537a2be89ff
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-09-26 22:45:24 +0800

    save!

--- main.go
+++ main.go
@@ -51,17 +51,14 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			if _, err := bot.PushMessage(prof, linebot.NewTemplateMessage("Smart AP <3", follow)).Do(); err != nil {
 					log.Print(err)
 			}
-
+		}
+		if event.Type == linebot.EventTypePostback {
 			data := event.Postback.Data
 			if data == "AddMAC"{
-				 if _, err := bot.PushMessage(prof, linebot.NewTextMessage("現在請輸入你的MAC：")).Do(); err != nil {
-	 					log.Print(err)
-	 			}
+				bot.PushMessage(prof, linebot.NewTextMessage("現在請輸入你的MAC：")).Do()
 			}
 			if data == "ModifyMAC"{
-				if _, err := bot.PushMessage(prof, linebot.NewTextMessage("現在請輸入你要更正的MAC：")).Do(); err != nil {
-						log.Print(err)
-				}
+				bot.PushMessage(prof, linebot.NewTextMessage("現在請輸入你要更正的MAC：")).Do()
 			}
 		}
 	}
